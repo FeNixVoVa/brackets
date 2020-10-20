b1d97b15718fab1155c5fa8a9b44512a77eb1860
FeNixVoVa <FeNixVoVa@gmail.com>
2020-10-20 23:30:27 +0300

    feat: complete task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let currentBrackets = '';
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if ((str[i] === bracketsConfig[j][0]) && (bracketsConfig[j][0] !== bracketsConfig[j][1])) {
+                currentBrackets += str[i];
+            } else if (((str[i] === bracketsConfig[j][0]) && (bracketsConfig[j][0] === bracketsConfig[j][1]))) {
+                if (currentBrackets[currentBrackets.length - 1] === bracketsConfig[j][1]) {
+                    currentBrackets = currentBrackets.slice(0, -1);
+                } else {
+                    currentBrackets += str[i];
+                }
+            } else if (str[i] === bracketsConfig[j][1]) {
+                if (currentBrackets[currentBrackets.length - 1] === bracketsConfig[j][0]) {
+                    currentBrackets = currentBrackets.slice(0, -1);
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    if (currentBrackets === '') return true;
+    return false;
 }
